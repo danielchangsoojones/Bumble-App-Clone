56e61d1408f1545910f6248c4baeb1b1806b1ea9
Daniel Jones <messyjones@gmail.com>
2016-12-05 10:17:57 -0500

    Initial Commit

---
+++ BumbleTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BumbleTesting
+//
+//  Created by Daniel Jones on 12/5/16.
+//  Copyright © 2016 Daniel Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
